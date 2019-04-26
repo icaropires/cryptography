066e6db1019db4136f3c18d1c7d058debc3b2212
joao robson <joaorobsonmartins@gmail.com>
2019-04-26 20:25:15 -0300

    Fix key shifiting

--- des/lib/des.ex
+++ des/lib/des.ex
@@ -17,6 +17,62 @@ defmodule DES do
   @block_size_bytes 8
   @num_rounds 16
 
+  @p [
+    16, 7, 20, 21, 29, 12, 28, 17,
+    1, 15, 23, 26, 5, 18, 31, 10,
+    2, 8, 24, 14, 32, 27, 3, 9,
+    19, 13, 30, 6, 22, 11, 4, 25
+  ]
+
+  @s_box [
+    [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+     [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+     [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+     [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13],
+    ],
+
+    [[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+     [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+     [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+     [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9],
+    ],
+
+    [[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+     [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+     [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+     [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12],
+    ],
+
+    [[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+     [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+     [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+     [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14],
+    ],
+
+    [[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+     [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+     [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+     [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3],
+    ],
+
+    [[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+     [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+     [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+     [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13],
+    ],
+
+    [[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+     [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+     [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+     [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12],
+    ],
+
+    [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+     [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+     [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+     [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11],
+    ]
+  ]
   @cp_1 [
     57, 49, 41, 33, 25, 17, 9,
     1, 58, 50, 42, 34, 26, 18,
@@ -26,6 +82,15 @@ defmodule DES do
     7, 62, 54, 46, 38, 30, 22,
     14, 6, 61, 53, 45, 37, 29,
     21, 13, 5, 28, 20, 12, 4
+  ]
+
+  @cp_2 [
+    14, 17, 11, 24, 1, 5, 3, 28,
+    15, 6, 21, 10, 23, 19, 12, 4,
+    26, 8, 16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55, 30, 40,
+    51, 45, 33, 48, 44, 49, 39, 56,
+    34, 53, 46, 42, 50, 36, 29, 32
   ]
 
   @e [
@@ -97,19 +162,36 @@ defmodule DES do
     permute block, @e
   end
 
+  defp shifts(list, left, right, index \\ 0)
+
+  defp shifts(list, left, right, index) when index == (length @shift_order) do 
+    list 
+  end
+
+  defp shifts(list, left, right, index) do
+    left = shift(left, Enum.at(@shift_order, index)) 
+    right = shift(right, Enum.at(@shift_order, index)) 
+    list = list ++ [left ++ right]
+    shifts(list, left, right, index + 1)
+
+  end
+
   @spec generate_keys([integer()]) :: [integer()]
   defp generate_keys(key) do
     k = permute(key, @cp_1, false)
+    list = []
     left = Enum.slice(k,0..27)
     right = Enum.slice(k,28..56)
-    keys = for {s, index} <- (Enum.with_index @shift_order), do: shift(left, Enum.at(@shift_order, index)) ++ shift(right, Enum.at(@shift_order, index))
-    a = Enum.map(keys, fn x -> Enum.map(Enum.chunk_every(x, @block_size_bytes), fn t -> String.to_integer(Enum.join(t),2) end) end)
-    a
+    keys = shifts(list, left, right) 
+    IO.puts(length keys)
+    h = Enum.map(keys, fn i -> Enum.map(@cp_2, fn x -> Enum.at(i, x - 1) end)end)
+
+    Enum.map(h, fn x -> Enum.map(Enum.chunk_every(x, @block_size_bytes), fn t -> String.to_integer(Enum.join(t),2) end) end)
   end
 
   @spec shift([String.t()], integer()) :: [integer()]
   defp shift(block, shift_size) do
-    Enum.slice(block,shift_size..28) ++ Enum.slice(block,0..(shift_size-1))
+    Enum.slice(block,shift_size..27) ++ Enum.slice(block,0..(shift_size-1))
   end
 
   @spec to_binary_list_string(charlist()) :: [String.t()]
@@ -186,6 +268,7 @@ defmodule DES do
     {left, right} = split_block(block)
     d_e = expansion(right)
     tmp = xor Enum.at(keys, n), d_e
+    #tmp = permute tmp, @p
     tmp = xor left, tmp
     left = right 
     right = tmp
@@ -195,9 +278,6 @@ defmodule DES do
 
   defp encrypt_block(block, keys, n) when n == @num_rounds do
     {left, right} = split_block(block)
-    IO.puts "+================"
-    IO.puts (length block)
-    IO.puts "+================"
     final_permutation (right ++ left) 
   end
 
@@ -206,6 +286,7 @@ defmodule DES do
     d_e = expansion(right)
     
     tmp = xor Enum.at(keys, n), d_e
+    #tmp = permute tmp, @p
     tmp = xor left, tmp
     left = right 
     right = tmp
@@ -229,6 +310,7 @@ defmodule DES do
     d_e = expansion(right)
     
     tmp = xor Enum.at(keys, 15-n), d_e
+    #tmp = permute tmp, @p
     tmp = xor left, tmp
     left = right 
     right = tmp
@@ -249,6 +331,7 @@ defmodule DES do
     d_e = expansion(right)
     
     tmp = xor Enum.at(keys, 15-n), d_e
+    #tmp = permute tmp, @p
     tmp = xor left, tmp
     left = right 
     right = tmp
