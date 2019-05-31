3d2db9d99aa862f3c38b5f7a57430657adbfda88
Ícaro Pires <icaropsa@gmail.com>
2019-05-31 19:08:22 -0300

    Complete salsa20
    Co-authored-by: João Robson <joaorobsonmartins@gmail.com>

--- prng_salsa20/prng_salsa20.go
+++ prng_salsa20/prng_salsa20.go
@@ -1,22 +1,29 @@
 package main
 
-import "fmt"
+import (
+	//"encoding/base64"
+	"encoding/binary"
+
+	//"fmt"
+	"io/ioutil"
+)
 
 const ROUNDS = 20
 
-func rotl(a, b byte) byte {
-	return (a << b) | (a >> (8 - b))
+func rotl(x, n uint32) uint32 {
+	x &= 0xffffffff
+	return (x << n) | (x>>(32-n))&0xffffffff
 }
 
-func quarter_round(a, b, c, d *byte) {
+func quarter_round(a, b, c, d *uint32) {
 	*b ^= rotl(*a+*d, 7)
 	*c ^= rotl(*b+*a, 9)
 	*d ^= rotl(*c+*b, 13)
 	*a ^= rotl(*d+*c, 18)
 }
 
-func salsa20_block(out, in []byte) {
-	x := make([]byte, 16)
+func salsa20_block(in []uint32) []uint32 {
+	x := make([]uint32, 16)
 
 	for i := range x {
 		x[i] = in[i]
@@ -30,20 +37,65 @@ func salsa20_block(out, in []byte) {
 
 		quarter_round(&x[0], &x[1], &x[2], &x[3])
 		quarter_round(&x[5], &x[6], &x[7], &x[4])
-		quarter_round(&x[10], &x[11], &x[8], &x[0])
+		quarter_round(&x[10], &x[11], &x[8], &x[9])
 		quarter_round(&x[15], &x[12], &x[13], &x[14])
 	}
 
-	for i := 0; i < 16; i++ {
+	out := make([]uint32, 16)
+	for i := range x {
 		out[i] = x[i] + in[i]
 	}
+
+	return out
+}
+
+func get_block(nonce, pos, key []uint32) []byte {
+	state := make([]uint32, 0, 16)
+	cons := make([]uint32, 4)
+
+	// cons = expand 32-byte key
+	cons[0], cons[1], cons[2], cons[3] = 1634760805, 857760878, 2036477234, 1797285236
+
+	// Fill salsa state
+	state = append(state, cons[0], key[0], key[1], key[2])
+	state = append(state, key[3], cons[1], nonce[0], nonce[1])
+	state = append(state, pos[0], pos[1], cons[2], key[4])
+	state = append(state, key[5], key[6], key[7], cons[3])
+
+	state = salsa20_block(state)
+
+	salsa20_block_bytes := make([]byte, 16*4)
+	for i, word := range state {
+		binary.LittleEndian.PutUint32(salsa20_block_bytes[i*4:i*4+4], word)
+	}
+
+	return salsa20_block_bytes
+}
+
+func stream_salsa20(length int, nonce, key []byte) []byte {
+	nonce_ints := make([]uint32, 0, len(nonce)/4)
+	key_ints := make([]uint32, 0, len(key)/4)
+
+	for i := 0; i < len(nonce)/4; i++ {
+		nonce_ints = append(nonce_ints, binary.LittleEndian.Uint32(nonce[i*4:i*4+4]))
+	}
+
+	for i := 0; i < len(key)/4; i++ {
+		key_ints = append(key_ints, binary.LittleEndian.Uint32(key[i*4:i*4+4]))
+	}
+
+	output := make([]byte, 0, length)
+	for i := 0; i < (length+63)/64; i++ {
+		output = append(output, get_block(nonce_ints, []uint32{uint32(i & 0xffffffff), uint32(i >> 32)}, key_ints)...)
+	}
+
+	return output
 }
 
 func main() {
-	out := make([]byte, 16)
-	in := []byte("abobora com pato")
+	key := make([]byte, 32)
+	out := stream_salsa20(512, []byte{4, 2, 4, 2, 4, 2, 4, 2}, key)
 
-	salsa20_block(out, in)
-
-	fmt.Println(out)
+	ioutil.WriteFile("out", out, 0644)
+	//fmt.Println(base64.StdEncoding.EncodeToString(out))
 }
