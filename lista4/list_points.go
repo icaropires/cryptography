5bbca361433632dea0440707428c01d74ecb5c0a
Ícaro Pires <icaropsa@gmail.com>
2019-06-30 21:25:51 -0300

    Add points sum over Zp

--- lista4/list_points.go
+++ lista4/list_points.go
@@ -13,9 +13,60 @@ type Point struct {
 	y *big.Int
 }
 
+func (p_point Point) String() string {
+	return fmt.Sprintf("(%s, %s)", p_point.x.String(), p_point.y.String())
+}
+
+func (p_point *Point) Add(q *Point, p *big.Int, a *big.Int) Point {
+	if p_point.x == q.x && p_point.y == new(big.Int).Neg(q.y) {
+		fmt.Println("Isso aqui vai dar infinito")
+		return Point{}
+	}
+
+	var lambda *big.Int
+	if p_point.x.Cmp(q.x) == 0 && p_point.y.Cmp(q.y) == 0 {
+		dividendAux := new(big.Int).Exp(p_point.x, big.NewInt(2), nil)
+		dividendAux = new(big.Int).Mul(big.NewInt(3), dividendAux)
+
+		dividend := new(big.Int).Add(dividendAux, a)
+		dividend = new(big.Int).Mod(dividend, p)
+
+		divisor := new(big.Int).Mul(big.NewInt(2), p_point.y)
+		divisor = new(big.Int).ModInverse(divisor, p)
+
+		lambdaAux := new(big.Int).Mul(dividend, divisor)
+		lambda = new(big.Int).Mod(lambdaAux, p)
+	} else {
+		deltaY := new(big.Int).Sub(q.y, p_point.y)
+		deltaX := new(big.Int).Sub(q.x, p_point.x)
+
+		deltaY = new(big.Int).Mod(deltaY, p)
+		deltaX = new(big.Int).ModInverse(deltaX, p)
+
+		if deltaX == nil {
+			fmt.Println("Zero division!")
+			return Point{}
+		}
+
+		lambdaAux := new(big.Int).Mul(deltaY, deltaX)
+		lambda = new(big.Int).Mod(lambdaAux, p)
+	}
+
+	xAux := new(big.Int).Exp(lambda, big.NewInt(2), nil)
+	xAux = new(big.Int).Sub(xAux, p_point.x)
+	xAux = new(big.Int).Sub(xAux, q.x)
+	x := new(big.Int).Mod(xAux, p)
+
+	yAux := new(big.Int).Mul(lambda, new(big.Int).Sub(p_point.x, x))
+	yAux = new(big.Int).Sub(yAux, p_point.y)
+	y := new(big.Int).Mod(yAux, p)
+
+	return Point{x, y}
+}
+
 // Returns y^2
 func curve(a, b, x *big.Int) *big.Int {
-	aux := new(big.Int).Exp(x, new(big.Int).SetUint64(3), nil)
+	aux := new(big.Int).Exp(x, big.NewInt(3), nil)
 
 	aux.Add(aux, new(big.Int).Mul(a, x))
 	aux.Add(aux, b)
@@ -26,7 +77,7 @@ func curve(a, b, x *big.Int) *big.Int {
 func ListCurvePoints(a, b, p *big.Int) []Point {
 	points := make([]Point, 0)
 	for xInt := int64(0); xInt < p.Int64(); xInt++ {
-		x := new(big.Int).SetInt64(xInt)
+		x := big.NewInt(xInt)
 
 		ySquare := curve(a, b, x)
 		y := new(big.Int).ModSqrt(ySquare, p)
@@ -40,6 +91,13 @@ func ListCurvePoints(a, b, p *big.Int) []Point {
 	}
 
 	return points
+}
+
+func getPoint(x, y int64) Point {
+	return Point{
+		big.NewInt(x),
+		big.NewInt(y),
+	}
 }
 
 func main() {
@@ -58,6 +116,13 @@ func main() {
 
 	fmt.Println("Total de pontos: ", len(points))
 	for i, point := range points {
-		fmt.Printf("Point #%d: (%s, %s)\n", i+1, point.x.String(), point.y.String())
+		fmt.Printf("Point #%d: %s\n", i+1, point)
 	}
+
+	p_point := getPoint(3, 10)
+	q := getPoint(9, 7)
+
+	fmt.Printf("%s + %s = ", p_point, q)
+	r := p_point.Add(&q, p, a)
+	fmt.Println(r)
 }
